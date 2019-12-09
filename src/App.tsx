6abe53a62acf15b6c13a12a8624768eaf13885c4
Marc Stauffer <marc.stauffer@capgemini.com>
2019-12-09 22:30:41 +0100

    feat: add basic folder structure and initial styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Main from './pages/main/index';
+import Main from './views/main/index';
 
 const App = () => <Main />;
 
---
+++ src/styles/base.js
@@ -0,0 +1,11 @@
+import { Dimensions } from 'react-native';
+
+export const dimensions = {
+  fullHeight: Dimensions.get('window').height,
+  fullWidth: Dimensions.get('window').width,
+};
+
+export const colors = {
+  ice: '#00DECC',
+  eggplant: '#4634AC',
+};
