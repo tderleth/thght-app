8070331c238ca18b4c8e7c4662669572bce1fe8a
Marc Stauffer <marc.stauffer@capgemini.com>
2019-12-10 20:55:53 +0100

    feat: add initial color variables

--- src/styles/base.tsx
+++ src/styles/base.tsx
@@ -1,9 +1,17 @@
+/* tslint:disable:object-literal-sort-keys */
 export const colors = {
+  white: '#fff',
+  black: '#000',
+  gray_light: '#222424',
+  gray_lighter: '#2E3437',
+  gray_lightest: '#8E8E93',
   eggplant: '#4634AC',
   ice: '#00DECC',
+  gradient_ice:
+    'linear-gradient(180deg, rgba(0, 0, 0, 0.2) 0%, rgba(0, 0, 0, 0) 100%), linear-gradient(180deg, #00DECC 0%, rgba(70, 52, 172, 0) 100%), #0D0D0D',
+  gradient_dark: 'linear-gradient(180deg, #08090A 0%, #1F2326 100%)',
 };
 
-/* tslint:disable:object-literal-sort-keys */
 export const padding = {
   xxs: 4,
   xs: 8,
