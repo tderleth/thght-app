db1ecabdaf2430af45ccaf03ca3d43894c3856ba
tderleth <thomas.derleth@me.com>
2019-12-13 12:30:05 +0100

    feat: add coverage step

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   collectCoverage: true,
   coverageReporters: ['lcov'],
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
   preset: 'react-native',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/dist/', '/node_modules/'],
