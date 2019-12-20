933e0868b731164aa03205eb884d289e291f08e5
tderleth <thomas.derleth@me.com>
2019-12-20 17:55:04 +0100

    fix: linting in jest config

--- jest.config.js
+++ jest.config.js
@@ -2,20 +2,20 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/styles/**/*.{js,jsx,ts,tsx}'],
   coverageReporters: ['lcov'],
-  preset: 'react-native',
-  testEnvironment: 'node',
-  testPathIgnorePatterns: ['/dist/', '/node_modules/'],
-  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
-  transform: {
-    '^.+\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  moduleFileExtensions: ['js', 'ts', 'tsx'],
   globals: {
     'ts-jest': {
       tsConfig: {
         jsx: 'react',
       },
     },
+  },
+  moduleFileExtensions: ['js', 'ts', 'tsx'],
+  preset: 'react-native',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+  testPathIgnorePatterns: ['/dist/', '/node_modules/'],
+  transform: {
+    '^.+\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
+    '^.+\\.tsx?$': 'ts-jest',
   },
 };
